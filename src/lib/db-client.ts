0645fdcba400e17e084e238d6a4d64a986a85786
Tiina Koskiranta <tiina.koskiranta@gmail.com>
2026-02-11 14:56:10 +0200

    fix local db connection
    - localhost did not work with SAM

--- src/lib/db-client.ts
+++ src/lib/db-client.ts
@@ -3,17 +3,22 @@
  * Creates clients configured for local or AWS environments
  */
 
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { env } from './env';
+import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
+import {DynamoDBDocumentClient} from '@aws-sdk/lib-dynamodb';
+import {env} from './env';
 
 export function createDynamoDBClient(): DynamoDBDocumentClient {
   const useLocalDb = env.USE_LOCAL_DB === 'true';
 
+  let endpoint = 'http://localhost:8000';
+  if(process.env.AWS_SAM_LOCAL) {
+    endpoint = 'http://dynamodb-local:8000'
+  }
+
   const client = new DynamoDBClient({
     region: env.AWS_REGION,
     ...(useLocalDb && {
-      endpoint: 'http://localhost:8000',
+      endpoint,
       credentials: {
         accessKeyId: 'local',
         secretAccessKey: 'local',
